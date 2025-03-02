77c88cb37b4e152905429248112f13c1e6be37cf
zm118 <59522095+zm118@users.noreply.github.com>
2025-03-02 13:15:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log(JSON.stringify({ time: new Date().toISOString() }));
+const fs = require('fs');
+
+const timeData = { time: new Date().toISOString() };
+fs.writeFileSync('time.json', JSON.stringify(timeData, null, 2));
